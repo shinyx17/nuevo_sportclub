5bc31183f645400c590550379d103a4c0a28a428
Erik <154481125+shinyx17@users.noreply.github.com>
2026-07-08 21:41:21 -0400

    eliminar backend antiguo

--- Backend/docker/mysql/init/01-init.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE IF NOT EXISTS frontend_users_db;
-
-CREATE USER IF NOT EXISTS 'club_user'@'%' IDENTIFIED BY 'club_pass';
-
-GRANT ALL PRIVILEGES ON frontend_users_db.* TO 'club_user'@'%';
-
-FLUSH PRIVILEGES;
